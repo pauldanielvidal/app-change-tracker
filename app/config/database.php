b0f5d1c0a85e0a84effc8f094093f3fd95c3d3a4
pauldanielvidal <paul.daniel.r.vidal@gmail.com>
2015-11-10 23:47:41 +0800

    no message

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 $container->setParameter('database_driver',       'pdo_mysql');
 
-$container->setParameter('database_host',         base64_decode( $_SERVER["RDS_HOSTNAME"])    );
-$container->setParameter('database_port',         base64_decode( $_SERVER["RDS_HOSTPORT"])    );
-$container->setParameter('database_name',         base64_decode( $_SERVER["RDS_DATABASE"])    );
-$container->setParameter('database_user',         base64_decode( $_SERVER["RDS_USERNAME"])    );
-$container->setParameter('database_password',     base64_decode( $_SERVER["RDS_PASSWORD"])    );
+$container->setParameter('database_host',         base64_decode( $_SERVER["RDS__HOSTNAME"])    );
+$container->setParameter('database_port',         base64_decode( $_SERVER["RDS__HOSTPORT"])    );
+$container->setParameter('database_name',         base64_decode( $_SERVER["RDS__DATABASE"])    );
+$container->setParameter('database_user',         base64_decode( $_SERVER["RDS__USERNAME"])    );
+$container->setParameter('database_password',     base64_decode( $_SERVER["RDS__PASSWORD"])    );
